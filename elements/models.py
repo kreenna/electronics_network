b18a2e0259ca0c6860029903f018675fe4dd2fbe
Arina <rudenskaaarina@gmail.com>
2025-10-06 20:28:55 +0300

    contact info and products

---
+++ elements/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class ContactInfo(models.Model):  # контакты звена сети
+    email = models.EmailField()
+    country = models.CharField(max_length=100, verbose_name="Страна")
+    city = models.CharField(max_length=100, verbose_name="Город")
+    street = models.CharField(max_length=100, verbose_name="Улица")
+    house_number = models.CharField(max_length=20, verbose_name="Дом")
+
+    class Meta:
+        abstract = True
+
+
+class Product(models.Model):  # сам продукт
+    name = models.CharField(max_length=255, verbose_name="Название")
+    model = models.CharField(max_length=255, verbose_name="Модель")
+    release_date = models.DateField(verbose_name="Дата выхода на рынок")
+
+    def __str__(self):
+        return f"{self.name} ({self.model})"
