a2274f5ce1a4f97c494619b61d0453b2a1b9733b
Arina <rudenskaaarina@gmail.com>
2025-10-07 19:08:41 +0300

    viewsets for the main models created

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
     'elements',
 ]
 
@@ -30,6 +31,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
     ],
 }
 
